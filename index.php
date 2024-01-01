92a871cf5e7420c15c6da4e6ea0fb6e36bb1c7d7
AndyDevX <andresag253@gmail.com>
2024-01-01 14:35:37 -0600

    Se agrega la librería Bootstrap, así como la creación de algunos directorios de trabajo

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
 		<meta  name="description" content="An interactive map that allows you to navigate through historical battle sites."/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Battle Mapping</title>
+		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css"> <!-- Bootstrap -->
 	</head>
+
+	<?php
+		#Navbar
+		include('nav.php');
+	?>
+
 	<body>
-		<h1>Battle Mapping!</h1>
+		
+	<div class="container-fluid">
+		Hola
+	</div>
+
 	</body>
 </html>
