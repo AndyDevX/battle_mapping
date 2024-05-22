1742144ec497128588df5321b02e3bed1f91d95a
AndyDevX <andresag253@gmail.com>
2024-05-21 19:32:47 -0600

    Instalación de Vue.js

---
+++ battle-mapping/src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="my-component">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            message: 'Hello world with Vue!'
+        }
+    }
+}
+</script>
